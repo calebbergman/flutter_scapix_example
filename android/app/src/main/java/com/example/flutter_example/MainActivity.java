01604de8eb08b91c3feca767baa5346e5353bf18
Caleb Bergman <cbergman@trans-system.com>
2020-01-24 08:56:28 -0800

    fix: add reference for MethodChannel

--- android/app/src/main/java/com/example/flutter_example/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_example/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.flutter_example;
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 import com.example.flutter_example.Person;
